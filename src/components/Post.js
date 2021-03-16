7cd2c288efc63c80a38b220ee7391d6c2a4a2b52
gsjuradog <gsjuradog@unal.edu.co>
2021-03-16 16:13:23 +0100

    Style media Queries for over 600px

--- src/components/Post.js
+++ src/components/Post.js
@@ -24,22 +24,21 @@ export default function Post({post}) {
         <img className="post-image" src={post.image} alt={altText} />
       </section>
       <div className='post-content'>
-      <div className="owner-info">
-      <div className="owner-avatarNname">
-        <img className="owner-image" src={post.owner.picture} alt={`${post.owner.firstName}'s avatar`} />
-        <p>{`${post.owner.firstName} ${post.owner.lastName}`}</p> 
-      </div>
-        <p><a href={`mailto:${post.owner.email}`}><img src='/mail.png' alt="contact e-mail"/></a></p>
-      </div>
-      <section className='post-text'>
-        <article>
-          <h5>{post.text}</h5>
-        </article>
-      </section>
+        <div className="owner-info">
+          <div className="owner-avatarNname">
+            <img className="owner-image" src={post.owner.picture} alt={`${post.owner.firstName}'s avatar`} />
+            <p>{`${post.owner.firstName} ${post.owner.lastName}`}</p> 
+          </div>
+            <div><a href={`mailto:${post.owner.email}`}><img className="owner-mail-icon" src='/mail.png' alt="contact e-mail"/></a></div>
+        </div>
+        <section className='post-text'>
+          <article>
+            <p>{post.text}</p>
+          </article>
+        </section>
       </div>
       <footer className="post-info">
         <span><img src='/heart.png' alt='likes heart' width={10} height={10}/><span>{post.likes}</span></span>
-        
         <span>{beutifyPublicationDate}</span>
         <span>
         {tagsWithId.map(tag=><span key={tag.id}><span >#{tag.title} </span></span>)}
